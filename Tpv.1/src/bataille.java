764451fecf45a42f0312174d4db520ea02ae79ab
Felipe_Maison <portugalfelipe8@gmail.com>
2023-02-22 13:37:08 -0500

    fct interchangeble dans un fichier texte

--- Tpv.1/src/bataille.java
+++ Tpv.1/src/bataille.java
@@ -8,58 +8,47 @@ public class bataille {
     public static int [][] grilleJeu = new int [10][10];
 
     public static boolean posOk(int [][]grille, int l, int c, int d, int t){
-        int valeurTab = 0;
+    int valeurTab = 0;
 
-        if(d ==1){
-            if(c > 5){
-                for(int indiceLigne = 0; indiceLigne < l; indiceLigne++){
-                    for(int indiceColonne = c; indiceColonne > (c-t);indiceColonne-- ){
-                        valeurTab+= grille[indiceLigne][indiceColonne];
-                    }
-                }
-            }
-            else{
-                for(int indiceLigne = 0; indiceLigne < l; indiceLigne++){
-                    for(int indiceColonne = c; indiceColonne < (c+t);indiceColonne++  ){
-                        valeurTab+= grille[indiceLigne][indiceColonne];
-                    }
+    if (d == 1) {
+        if (c + t > 9) {
+            return false; // Vérifie si la ligne dépasse, si oui rtourne faux
+        }
+        for (int indiceLigne = 0; indiceLigne < 9; indiceLigne++) {
+            for (int indiceColonne = c; indiceColonne < c + t; indiceColonne++) {
+                if (indiceLigne < l || indiceLigne >= l + t) {
+                    valeurTab += grille[indiceLigne][indiceColonne];
                 }
             }
         }
-        else{
-            if(l > 5){
-                for(int indiceLigne = l; indiceLigne > (l-t); indiceLigne--){
-                    for(int indiceColonne = 0; indiceColonne < c; indiceColonne++){
-                        valeurTab+= grille[indiceLigne][indiceColonne];
-
-                    }
-                }
-            }
-            else{
-                for(int indiceLigne = l; indiceLigne > (l+t); indiceLigne++){
-                    for(int indiceColonne = 0; indiceColonne < c; indiceColonne++){
-                        valeurTab+= grille[indiceLigne][indiceColonne];
-
-                    }
+    } else {
+        if (l + t > grille.length) {
+            return false; // Vérifie si la colonne dépasse, si oui retourne faux
+        }
+        for (int indiceLigne = l; indiceLigne < l + t; indiceLigne++) {
+            for (int indiceColonne = 0; indiceColonne < 9; indiceColonne++) {
+                if (indiceColonne < c || indiceColonne >= c + t) {
+                    valeurTab += grille[indiceLigne][indiceColonne];
                 }
             }
         }
-        System.out.println(valeurTab);
-        if(valeurTab == 0){
-            return true;
+    }
 
-        }
-        else{
-            return false;
-        }
+    if(valeurTab == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+
     }
     public static void initGrilleOrdi(){
         int l = randRange(0, 10);
         int c = randRange(0, 10);
         int d = randRange(1, 3);
-        //En verticale
+
         System.out.println("L: "+ (l ) + " C: " + (c ) + " D: " +d);
-//pourrais mettre l'intiation  des ranb a l'interierur de bato
+
         bato(l,c, d, 5);
         l = randRange(0, 10);
         c = randRange(0, 10);
@@ -91,12 +80,19 @@ public class bataille {
         boolean ok = true;
 
         do{
-            //for(int indice = 0; indice < lngr; indice++){
-                ok = posOk(grilleOrdi,ligne, colonne, direction, 5);
-            //}
+            for(int indice = 0; indice < lngr; indice++){
+                ok = posOk(grilleOrdi,ligne, colonne, direction, lngr);
+//                if(ok == false ){
+//                    ligne = randRange(0, 10);
+//                    colonne = randRange(0, 10);
+//                    direction = randRange(1, 3);
+//                    System.out.println("La ligne est: "+ (ligne + 1) + "\nColonne: " + (colonne + 1) + "\nDirection: " +direction);
+//                    break;
+//                }
+            }
             if(ok == false ){
-                ligne = randRange(0, 10);
-                colonne = randRange(0, 10);
+                ligne = randRange(0, 9);
+                colonne = randRange(0, 9);
                 direction = randRange(1, 3);
                 System.out.println("La ligne est: "+ (ligne + 1) + "\nColonne: " + (colonne + 1) + "\nDirection: " +direction);
             }
